f0abbc74d3e8f84a8bb053d58ed20209395057ac
Atul kumar singh <atul.11192@gmail.com>
2019-10-25 10:08:44 +0530

    uploading the production app

--- routes/userFunctions.js
+++ routes/userFunctions.js
@@ -2,6 +2,7 @@ const express=require('express');
 const user=require('../models/User');
 const bcrypt=require('bcryptjs');
 const jwt=require('jsonwebtoken');
+const order=require('../models/Orders');
 const router=express.Router();
 
 router.post('/register',(req,res)=>{
@@ -58,6 +59,50 @@ router.post('/login',(req,res)=>{
       res.status(403).json({
         message:'Oops error occured',
         err:err      });
+  });
+});
+router.get('/details/:email',(req,res)=>{
+  const Email=req.params.email;
+  user.findOne({where:{Email}}).then(user=>{
+    res.status(200).json({user});
+    }).catch(err=>{
+     res.status(405).json({
+       message:'Error occured',
+       error:err
+     });
+  });
+});
+router.put('/:id',(req,res)=>{
+  const id=req.params.id;
+  const email=req.body.email;
+  const phone=req.body.phone;
+  const name=req.body.name;
+  user.update({
+    Email:email,
+    Phone:phone,
+    Name:name
+  },{where:{id}}).then(result=>{
+    res.status(200).json({
+      message:'Profile updated'
+    });
+  }).catch(err=>{
+      res.status(405).json({
+        message:'Error occured',
+        error:err
+      });
+    });
+});
+router.get('/order/:email',(req,res)=>{
+  const Email=req.params.email;
+  order.findAll({where:{Email}}).then(result=>{
+    res.status(200).json({
+      result
+    });
+  }).catch(err=>{
+    res.status(405).json({
+      message:'error occured',
+      error:err
+    });
   });
 });
 module.exports=router;
--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const user=require('./routes/userFunctions');
 const admin=require('./routes/adminFunctions');
 const product=require('./routes/productFunctions');
 const cart=require('./routes/cartFunctions');
+const path=require('path');
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
@@ -19,11 +20,15 @@ app.use((req, res, next) => {
     "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
   next();
-});
+});*/
+app.use('/',express.static(path.join(__dirname,'view')));
 app.use('/admin',admin);
 app.use('/user',user);
 app.use('/product',product);
 app.use('/cart',cart);
+app.use((req,res,next)=>{
+  res.sendFile(path.join(__dirname,'view','index.html'));
+});
 app.listen(PORT,()=>{
   console.log('server started on '+PORT);
 });
